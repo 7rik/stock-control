e69683f1fd73dced37795a1f360e2fb03453ead0
7rik <bezerra.erik2003@gmail.com>
2023-11-07 17:46:19 -0300

    implementadas validações nos formulários

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-  loginCard = true;
+  public loginCard = true;
+
+  public loginForm = this.formBuilder.group({
+    'email': ['', Validators.required],
+    'password': ['', Validators.required]
+  });
+
+  public signUpForm = this.formBuilder.group({
+    'name': ['', Validators.required],
+    'email': ['', Validators.required],
+    'password': ['', Validators.required]
+  });
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  public onSubmitLoginForm(): void {
+    console.log('DADOS DO FORMULÁRIO DE LOGIN: ', this.loginForm.value);
+  }
+
+  public onSubmitSignUpForm(): void {
+    console.log('DADOS DO FORMULÁRIO DE CADASTRO: ', this.signUpForm.value);
+  }
 }
